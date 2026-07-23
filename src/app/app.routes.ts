64ba331de2b4f71d3e10df46056a1d631f8eefe2
Muhammad Hadin Mirza <hadinmirza341@gmail.com>
2026-07-23 13:46:26 +0500

    Implement Angular routing with active link highlights and Route Paramters

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Home } from './Pages/home/home';
+import { Products } from './Pages/products/products';
+import { ProductDetails } from './Pages/product-details/product-details';
+import { About } from './Pages/about/about';
+import { Error } from './Pages/error/error';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home,
+    },
+    {
+        path: "products",
+        component: Products,
+    },
+    {
+        path: "products/:id",
+        component: ProductDetails,
+    },
+    {
+        path: 'about',
+        component: About,
+    },
+    {
+    path:'**',
+    component: Error,
+    }
+
+];
