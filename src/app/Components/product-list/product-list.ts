2b444d2dd116966d4c2e626ff1b9e91a91748daa
Muhammad Hadin Mirza <hadinmirza341@gmail.com>
2026-07-23 16:51:07 +0500

    Implement reusable product list and product cards

--- src/app/Components/product-list/product-list.ts
+++ src/app/Components/product-list/product-list.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductCard } from '../product-card/product-card';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [ProductCard, NgFor],
   templateUrl: './product-list.html',
-  styleUrl: './product-list.css',
+  styleUrl: './product-list.css'
 })
-export class ProductList {}
+export class ProductList {
+
+  @Input()
+  products: Product[] = [];
+
+}
